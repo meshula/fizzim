f18a111000da14ac2426b39386b2bcaecdca2463
meshula <>
2002-02-17 00:38:54 +0000

    no message

--- fizzim0_3/fizzim0_3/include/basictypes.h
+++ fizzim0_3/fizzim0_3/include/basictypes.h
@@ -13,8 +13,8 @@
 typedef unsigned char	uint8;
 typedef unsigned short	uint16;
 typedef unsigned int	uint32;
-typedef char			int8;
-typedef short			int16;
-typedef int				int32;
+typedef char			uint8;
+typedef short			uint16;
+typedef int				uint32;
 
 #endif

5df964dba1c890d9b199ee6fe385617780936d36
meshula <>
2003-10-11 06:02:32 +0000

    added core file

---
+++ PhysicsEngine/include/DynamicState.h
@@ -0,0 +1,49 @@
+
+/** @file	DynamicState.h
+	@brief	Instantaneous rigid body dynamic state
+*/
+
+
+/** The state of a rigid body
+ */
+
+class DynamicState
+{
+public:
+	DynamicState()
+	{
+		SetAtRest();
+	}
+
+	~DynamicState() { }
+
+	DynamicState& operator=(const DynamicState& right)
+	{	// assign _Right
+		if (this != &right) {
+			PMath::QuatSet(m_Orientation,		right.m_Orientation);
+			PMath::Vec3fSet(m_Position,			right.m_Position);
+			PMath::Vec3fSet(m_Velocity,			right.m_Velocity);
+			PMath::Vec3fSet(m_AngularVelocity,	right.m_AngularVelocity);
+			PMath::Vec3fSet(m_AngularMomentum,	right.m_AngularMomentum);
+		}
+		return (*this);
+	}
+
+	void	SetAtRest()
+	{
+		m_Orientation[0] = Real(0.0f);
+		m_Orientation[0] = Real(0.0f);
+		m_Orientation[0] = Real(0.0f);
+		m_Orientation[0] = Real(1.0f);
+		PMath::Vec3fZero(m_Position);
+		PMath::Vec3fZero(m_Velocity);
+		PMath::Vec3fZero(m_AngularVelocity);
+		PMath::Vec3fZero(m_AngularMomentum);
+	}
+
+	PMath::Quaternion	m_Orientation;
+	PMath::Vec3f		m_Position;
+	PMath::Vec3f		m_Velocity;
+	PMath::Vec3f		m_AngularVelocity;
+	PMath::Vec3f		m_AngularMomentum;
+};
